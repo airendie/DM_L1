d721952181d0795198ab6e247c5002ae4e7e4d31
airendie <maxim.zh2005@gmail.com>
2024-10-26 22:42:32 +0300

    CMake & Base classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "universum.hpp"
 
 int main()
 {
-    
+    int n;
+    std::cout << "Please, input n: ";
+    std::cin >> n;
+
+    Universum u(n);
+    u.print();
+
     return 1;
 }
